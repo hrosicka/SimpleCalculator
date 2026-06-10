75ce9ff67193d6a22b10f0a43161942e9b3edbfe
Hanka <hrosicka@post.cz>
2026-06-10 20:49:51 +0200

    style: format python code with black

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # config.py
 
 COLORS = {
-    'primary': '#0B132B',
-    'accent': '#7F2982',
-    'error': '#F7717D',
-    'background': '#D8D6E6',
-    'text': '#0B132B'
+    "primary": "#0B132B",
+    "accent": "#7F2982",
+    "error": "#F7717D",
+    "background": "#D8D6E6",
+    "text": "#0B132B",
 }
 
 
 # Constants for setup
-WINDOW_TITLE = 'PyQt Calculator'
+WINDOW_TITLE = "PyQt Calculator"
 DEFAULT_PRECISION = 5
 NUMBER_RANGE_MIN = -10000000
 NUMBER_RANGE_MAX = 10000000
-INITIAL_RESULT = '0.0'
+INITIAL_RESULT = "0.0"
 MAX_HISTORY_SIZE = 10 * 1024 * 1024  # 10MB limit
